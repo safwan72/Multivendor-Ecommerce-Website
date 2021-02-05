c0478766ac3c628b21e1148d802661ecf92cef12
safwan <safwanali137@gmail.com>
2021-02-06 00:39:26 +0600

    third commit

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=BASE_DIR/'templates'
@@ -22,12 +22,12 @@ MEDIA_DIR=BASE_DIR/'media'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-yuf6*#2yt!!y5(!++9&_3+wyhug67s64nxt$86oq62#5w*xgr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =config('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -89,7 +89,7 @@ INTERNAL_IPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('NAME'),
     }
 }
 
